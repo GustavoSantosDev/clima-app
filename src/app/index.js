2f7a8f975902576e8a54941789525b2137c48bc9
Gustavo Santos <jgustavo.santosc@gmail.com>
2021-01-04 22:28:52 -0600

    api conexion

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,11 @@
+const {Weather} = require('./weather');
+const weather = new Weather('London', 'uk')
 require('./index.css');
+
+async function fetchWeather() {
+   const response = await weather.getWeather();
+   const data = await response.json();
+   console.log(data);
+}
+
+document.addEventListener('DOMContentLoaded', fetchWeather)
